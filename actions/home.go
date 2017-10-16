f9e958cd39b0d3b3d0f9f709bd1c702cfa4b72e7
benji93 <benjamin.james.fell@gmail.com>
2017-10-16 14:51:17 +0100

    My code smells buffalo tutorial part 1

--- actions/home.go
+++ actions/home.go
@@ -5,5 +5,17 @@ import "github.com/gobuffalo/buffalo"
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-  return c.Render(200, r.HTML("index.html"))
-  }
+	return c.Render(200, r.HTML("home.html"))
+}
+
+// ResumeHandler will direct the user to the
+// resume page
+func ResumeHandler(c buffalo.Context) error {
+	return c.Render(200, r.HTML("resume.html"))
+}
+
+// ContactHandler will direct the user to the
+// contact page
+func ContactHandler(c buffalo.Context) error {
+	return c.Render(200, r.HTML("contact.html"))
+}
